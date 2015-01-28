5569a570caca5265be33a35342e373dede121354
phouse512 <philiphouse2015@u.northwestern.edu>
2015-01-28 14:20:11 -0600

    new configs

--- puzzle/config.py
+++ puzzle/config.py
@@ -2,7 +2,7 @@ import os
 PWD = os.path.abspath(os.curdir)
 
 DEBUG=True
-SQLALCHEMY_DATABASE_URI = 'postgres://eedfuqmgrbyzml:0o1RVkhp7l9FS94kyk3nqfozrs@ec2-54-204-40-96.compute-1.amazonaws.com:5432/d8uars1eukmnaa'
+SQLALCHEMY_DATABASE_URI = 'postgres://swbzjtpjzoakwk:eCZTiE0gr6VXURN9FTS38guxxX@ec2-54-235-193-41.compute-1.amazonaws.com:5432/d37fueml6m5cpe'
 #SQLALCHEMY_DATABASE_URI = 'postgres://PhilipHouse:house@localhost/puzzle'
 #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SECRET_KEY = 'thisissecret'
